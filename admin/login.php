c0a1c65b9dbd30e98816fab92d8dd01f5e9a0b34
gyaanii <gagboy717@gmail.com>
2017-10-13 02:28:31 +0545

    mainatin index page

--- admin/login.php
+++ admin/login.php
@@ -4,7 +4,57 @@
               $username =$_POST['username'];
               $password =$_POST['password'];
 
+
+	      require_once("includes/connection.php");
+	      $pdo = db_connect();
+
+        $query = "SELECT username, password, salt FROM login where username = :username";
+        $query_params = array(':username'=>$username);
+
+
+        //run the query against database
+        try{
+              $line = $pdo->prepare($query);
+              $result = $line->execute($query_params);
+        }catch(PDOException $e){
+                die("Failed to run query!".$e->getMessage());
         }
+
+
+        //setting the initial login status 
+        $login_status = false;
+
+        //get user data from database
+        $query_data = $line->fetch();
+
+
+        //comparing the password 
+        if($query_data){
+                $cpwd = hash('sha256',$password.$query_data['salt']);
+
+                for($times=0; $times< 65536; $times++){
+                        $cpwd = hash('sha256', $cpwd . $query_data['salt']);
+                }
+
+                if($cpwd === $query_data['password']){ $login_status = true; }
+        }
+
+
+
+        if($login_status){
+             unset($query_data['salt']);
+             unset($query_data['password']);
+
+             //storing user data in session varibale
+              $_SESSION['user'] = $query_data['username']; 
+
+             header('Location: home.php');
+             die("Redirecting to : home.php");
+        }else{
+             print("login failed ;( Try Again?!");
+        }
+
+
 
 
 ?>
